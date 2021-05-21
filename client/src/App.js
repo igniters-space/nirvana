cf6b9111e374d4679c77296f0c526ac5042b2262
sairaj <sairaj2119@gmail.com>
2021-05-21 18:47:42 +0530

    resolve username field to check on prod

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,21 @@ import axe from './utils/api'
 const App = () => {
   const eRef = useRef()
   const pRef = useRef()
+  const uRef = useRef()
   const handleSub = (e) => {
     e.preventDefault()
-    const user = { email: eRef.current.value, password: pRef.current.value }
-    const res = axe.post('/auth/signup', user, {})
+    const user = {
+      username: uRef.current.value,
+      email: eRef.current.value,
+      password: pRef.current.value,
+    }
+    const res = axe.post('/auth/signup', user)
     console.log(res.data)
   }
   return (
     <div>
       <form onSubmit={handleSub}>
+        <input type="text" defaultValue="saira" ref={uRef} />
         <input type="email" defaultValue="sairaj2119@gmail.com" ref={eRef} />
         <input type="password" defaultValue="asdfaa" ref={pRef} />
         <button>sumbit</button>
