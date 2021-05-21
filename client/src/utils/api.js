d8f07226a1e0af597111caf8be364bbaf61bac46
sairaj <sairaj2119@gmail.com>
2021-05-21 18:59:48 +0530

    conditionally change base url for of axios in prod

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? '/api'
+      : 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
