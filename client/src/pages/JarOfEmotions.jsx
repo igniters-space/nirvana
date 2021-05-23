b3af31382c5fbe689590a19929b82fe1a757f8eb
irsayvid <dsdarimisetti@gmail.com>
2021-05-23 12:58:30 +0530

    edit modal and jars

--- client/src/pages/JarOfEmotions.jsx
+++ client/src/pages/JarOfEmotions.jsx
@@ -5,17 +5,15 @@ import NoteModal from '../components/NoteModal'
 const JarOfEmotions = () => {
   return (
     <div className="jar">
-      <h1> Add your emotions to jar </h1>
+      <h1> Add your emotions in the jars </h1>
       <div className="jar__row">
         <div className="jar__column1">
         <img className="blueJar" src="/blue-jar.svg" alt="JarOfHappiness"/>
         <NoteModal Title = "Jar Of Happiness" titlePH="Title for description" descriptionPH="You can add your happy moments, quotes that inspire you, things you are grateful for or incidents that bring a smile on your face"/>
-        {/*<button type="button" className="blueJarBtn">Add note</button>*/}
          </div>
         <div className="jar__column2">
         <img className="redJar" src="/red-jar.svg" alt="JarOfSadness"/>
         <NoteModal Title = "Jar Of Sadness" titlePH="Relevant Title" descriptionPH="You can add few reasons for your failure or what you would like to learn and change from the incident"/>
-        {/*<button type="button" className="redJarBtn">Add note</button>*/}
         </div>
       </div>
     </div>
